6b2aed792d0ebc91e06cae783ce8d53318249197
Ben Langfeld <ben@langfeld.me>
2011-11-14 16:30:53 +0000

    Add a dialplan method for joining a MeetMe conference

--- lib/ahn_asterisk/plugin.rb
+++ lib/ahn_asterisk/plugin.rb
@@ -121,5 +121,30 @@ module AhnAsterisk
         end
       end
     end
+
+    #
+    # Used to join a particular conference with the MeetMe application. To use MeetMe, be sure you
+    # have a proper timing device configured on your Asterisk box. MeetMe is Asterisk's built-in
+    # conferencing program.
+    #
+    # @param [String] conference_id
+    # @param [Hash] options
+    #
+    # @see http://www.voip-info.org/wiki-Asterisk+cmd+MeetMe Asterisk Meetme Application Information
+    #
+    dialplan :meetme do |conference_id, options = {}|
+      conference_id = conference_id.to_s.scan(/\w/).join
+      command_flags = options[:options].to_s # This is a passthrough string straight to Asterisk
+      pin = options[:pin]
+      raise ArgumentError, "A conference PIN number must be numerical!" if pin && pin.to_s !~ /^\d+$/
+
+      # To disable dynamic conference creation set :use_static_conf => true
+      use_static_conf = options.has_key?(:use_static_conf) ? options[:use_static_conf] : false
+
+      # The 'd' option of MeetMe creates conferences dynamically.
+      command_flags += 'd' unless command_flags.include?('d') || use_static_conf
+
+      execute "MeetMe", conference_id, command_flags, options[:pin]
+    end
   end
 end
--- spec/ahn_asterisk/plugin_spec.rb
+++ spec/ahn_asterisk/plugin_spec.rb
@@ -108,5 +108,48 @@ module AhnAsterisk
         subject.sip_get_header("x-ahn-header").should == value
       end
     end
+
+    describe '#join' do
+      it "should pass the 'd' flag when no options are given" do
+        conference_id = "123"
+        subject.expects(:execute).once.with("MeetMe", conference_id, "d", nil)
+        subject.meetme conference_id
+      end
+
+      it "should pass through any given flags with 'd' appended to it if necessary" do
+        conference_id, flags = "1000", "zomgs"
+        subject.expects(:execute).once.with("MeetMe", conference_id, flags + "d", nil)
+        subject.meetme conference_id, :options => flags
+      end
+
+      it "should NOT pass the 'd' flag when requiring static conferences" do
+        conference_id, options = "1000", {:use_static_conf => true}
+        subject.expects(:execute).once.with("MeetMe", conference_id, "", nil)
+        subject.meetme conference_id, options
+      end
+
+      it "should raise an ArgumentError when the pin is not numerical" do
+        lambda {
+          subject.expects(:execute).never
+          subject.meetme 3333, :pin => "letters are bad, mkay?!1"
+        }.should raise_error ArgumentError
+      end
+
+      it "should strip out illegal characters from a conference name" do
+        bizarre_conference_name = "a-    bc!d&&e--`"
+        normal_conference_name = "abcde"
+        subject.expects(:execute).twice.with("MeetMe", normal_conference_name, "d", nil)
+
+        subject.meetme bizarre_conference_name
+        subject.meetme normal_conference_name
+      end
+
+      it "should allow textual conference names" do
+        lambda {
+          subject.expects(:execute).once
+          subject.meetme "david bowie's pants"
+        }.should_not raise_error
+      end
+    end
   end
 end
