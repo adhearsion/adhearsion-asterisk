41810afecc1c6d92cd16635246c2eb6d1e196b9f
Ben Langfeld <ben@langfeld.me>
2012-01-30 15:46:36 +0000

    [CS] Slight modifications to test structure

--- spec/adhearsion/asterisk/plugin_spec.rb
+++ spec/adhearsion/asterisk/plugin_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 module Adhearsion::Asterisk
   describe 'plugin loading' do
-    let(:mock_call) { stub_everything 'Call', :originating_voip_platform => :punchblock }
-    
+    before(:all) { Adhearsion::Plugin.init_plugins }
+
+    let(:mock_call) { stub_everything 'Call' }
+
     it 'should monkeypatch Adhearsion::CallController' do
       cc = Adhearsion::CallController.new(mock_call)
       cc.expects(:play_time).returns true
       cc.play
     end
-    
+
     it 'should extend Adhearsion::CallController with Asterisk methods' do
       Adhearsion::CallController.new(mock_call).should respond_to :play_time
     end
